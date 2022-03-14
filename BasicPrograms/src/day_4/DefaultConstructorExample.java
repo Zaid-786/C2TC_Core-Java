c339a311a8c2904fee80937e1d96ec5e203c4310
motiv <motiv@192.168.1.216>
2022-03-14 17:14:57 +0530

    oops and logic programs

---
+++ BasicPrograms/src/day_4/DefaultConstructorExample.java
@@ -0,0 +1,20 @@
+package day_4;
+
+public class DefaultConstructorExample
+{
+	DefaultConstructorExample()					//this is a defaultconstructor method its automatically created or called when we create a instance of any class
+	{
+		System.out.println("this is a defaultconstructor");
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		DefaultConstructorExample  a=new DefaultConstructorExample();
+	 
+		
+	}
+
+}
