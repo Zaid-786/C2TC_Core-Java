087e28640984d119c566e6609616919f0e591ee3
motiv <motiv@LAPTOP-874R7ID5>
2022-03-27 20:02:22 +0530

    hhshhs

---
+++ BasicPrograms/src/day_5/PrimeNumber_1_to_100.java
@@ -0,0 +1,35 @@
+package day_5;
+
+
+// Write a program to Prime Number 1 to 100 ?
+
+public class PrimeNumber_1_to_100 {
+
+	public static void main(String[] args) 
+	{
+		int count = 0;
+
+		System.out.println("prime number 1 to 100 ");
+
+		for (int i = 1; i <= 100; i++)
+		{
+			count = 0;
+			for (int j = 1; j <= i; j++) 
+			{
+				if (i % j == 0) 
+				{
+					count++;
+				}
+			}
+			
+			if (count == 2) 
+			{
+				System.out.print(i+" ");
+			}
+		}
+
+
+
+	}
+
+}
