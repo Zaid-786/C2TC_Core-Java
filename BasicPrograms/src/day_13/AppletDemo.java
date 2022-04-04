2a9a0e09906f2daeead01fe5fbe3d9e7bffa2ca6
motiv <motiv@LAPTOP-874R7ID5>
2022-04-04 08:07:12 +0530

    some Applet programs

---
+++ BasicPrograms/src/day_13/AppletDemo.java
@@ -0,0 +1,13 @@
+package day_13;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class AppletDemo extends Applet{
+	
+	public void paint(Graphics g)			//this is method of component class
+	{
+		g.drawString("hello java techie...",50,50);
+	}
+
+}
